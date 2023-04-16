3d0dd0b8423be5d317af2fb96d299c212cf5f892
Smxrfyy <stevenjamesl@hotmail.com>
2023-04-16 16:53:10 +0000

    Views Correctly Defined and Configured

--- djangoApps/itservices/itapps/itapps/settings.py
+++ djangoApps/itservices/itapps/itapps/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'itreporting.apps.ItreportingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
