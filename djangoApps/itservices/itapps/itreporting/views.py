61bedc6980ff2001dba0a2431c390f807c966596
Smxrfyy <stevenjamesl@hotmail.com>
2023-04-16 16:24:04 +0000

    Views Configured

--- djangoApps/itservices/itapps/itreporting/views.py
+++ djangoApps/itservices/itapps/itreporting/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return render(request, 'itreporting/home.html')
+
+def about(request):
+    return render(request, 'itreporting/about.html')
+
+def contact(request):
+    return render(request, 'itreporting/contact.html')
+
+def products(request):
+    return render(request, 'itreporting/products.html')
+
+def profile(request):
+    return render(request, 'itreporting/profile.html')
