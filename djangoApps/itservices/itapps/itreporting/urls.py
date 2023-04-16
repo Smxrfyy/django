6b5366babba7d3bd54735dce5a2b8be33c58a6c7
Smxrfyy <stevenjamesl@hotmail.com>
2023-04-16 17:43:45 +0000

    Headers and web pages styled and functional

--- djangoApps/itservices/itapps/itreporting/urls.py
+++ djangoApps/itservices/itapps/itreporting/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns =[
+path('home.html', views.home, name='itreporting-home'),
 path('', views.home, name='itreporting-home'),
-path('about/', views.about, name='itreporting-about'),
-path('contact/', views.contact, name='itreporting-contact'),
+path('about.html', views.about, name='itreporting-about'),
+path('contact.html', views.contact, name='itreporting-contact'),
+path('products.html', views.products, name='itreporting-products'),
+path('profile.html', views.profile, name='itreporting-profile'),
 
 ]
