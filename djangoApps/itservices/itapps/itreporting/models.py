601a5f5509bfeaedc667204622f5b8264ab6d2c9
Smxrfyy <stevenjamesl@hotmail.com>
2023-04-17 20:36:09 +0000

    Models created

--- djangoApps/itservices/itapps/itreporting/models.py
+++ djangoApps/itservices/itapps/itreporting/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class review(models.Model):
+    type = models.CharField(max_length = 256)
+    room = models.CharField(max_length = 256)
+    details = models.TextField()
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    date_submitted = models.DateTimeField(default = timezone.now)
+    
+
+    def __str__(self):
+        return self.type
+
+    def get_absolute_url(self):
+        return reverse('review-detail', kwargs = {'pk': self.pk})
