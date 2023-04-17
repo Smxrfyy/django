84f2cd550aed1452c75821bb3ea73be6bb521f4b
Smxrfyy <stevenjamesl@hotmail.com>
2023-04-17 20:35:24 +0000

    Fix for CSS not linking

--- djangoApps/itservices/itapps/itreporting/urls.py
+++ djangoApps/itservices/itapps/itreporting/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns =[
 path('home.html', views.home, name='itreporting-home'),
 path('', views.home, name='itreporting-home'),
@@ -7,5 +8,7 @@ path('about.html', views.about, name='itreporting-about'),
 path('contact.html', views.contact, name='itreporting-contact'),
 path('products.html', views.products, name='itreporting-products'),
 path('profile.html', views.profile, name='itreporting-profile'),
+
+
 
 ]
--- djangoApps/itservices/itapps/itreporting/views.py
+++ djangoApps/itservices/itapps/itreporting/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'itreporting/home.html')
+    return render(request, 'itreporting/home.html', {'title': 'Home Page'})
 
 def about(request):
-    return render(request, 'itreporting/about.html')
+    return render(request, 'itreporting/about.html', {'title': 'About Us'})
 
 def contact(request):
-    return render(request, 'itreporting/contact.html')
+    return render(request, 'itreporting/contact.html', {'title': 'Contact Us'})
 
 def products(request):
-    return render(request, 'itreporting/products.html')
+    return render(request, 'itreporting/products.html', {'title': 'Products'})
 
 def profile(request):
-    return render(request, 'itreporting/profile.html')
+    return render(request, 'itreporting/profile.html', {'title': 'Profile'})
+
