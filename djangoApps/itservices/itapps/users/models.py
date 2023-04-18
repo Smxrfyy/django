828ed251ccca9ddf65644141728fb3754952781f
Smxrfyy <stevenjamesl@hotmail.com>
2023-04-18 00:41:59 +0000

    profile page fully functional

--- djangoApps/itservices/itapps/users/models.py
+++ djangoApps/itservices/itapps/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Avg
+from django.urls import reverse
 
 # Create your models here.
 class Profile(models.Model):
